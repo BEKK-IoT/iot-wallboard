0880256b1110f23613a5f17e419b40cd4260747c
Birgitte Haavardsholm <birgitte haavardshol@mac-birgittehaavardsholm.local>
2015-06-03 17:02:41 +0200

    la til en sphere

--- js/components/webgl/webgltest.js
+++ js/components/webgl/webgltest.js
@@ -4,13 +4,22 @@ var webglWidget = function () {
 
 	var scene, camera, renderer;
 	var container;
+	var mesh;
 
 	var initGeometry = function(){
-		var geometry = new THREE.BoxGeometry( 20, 20, 20);
-        var material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
- 
-        var mesh = new THREE.Mesh( geometry, material );
-        scene.add( mesh );
+        var geometry = new THREE.SphereGeometry( 5, 32, 16 );
+		var material = new THREE.MeshLambertMaterial( { color: 0xEFFF45 } );
+		mesh = new THREE.Mesh( geometry, material );
+		scene.add(mesh);
+	};
+
+	var initLight = function(){
+		var light = new THREE.PointLight(0xffffff);
+		light.position.set(100,250,100);
+		scene.add(light);
+	    light = new THREE.PointLight(0xffffff);
+		light.position.set(100,-250,100);
+		scene.add(light);
 	};
 
 	var onWindowResize  = function( event ) {
@@ -36,8 +45,25 @@ var webglWidget = function () {
     		camera.position.set(0,0,50);
     		scene.add(camera);
 
+    		initLight();
     		initGeometry();
     		renderer.render( scene, camera );
+		},
+		makeSphereGlow : function(){
+			var material = new THREE.MeshLambertMaterial( { color: 0xFFB300 } );
+			mesh.materials = material;
+			// SUPER SIMPLE GLOW EFFECT
+			// use sprite because it appears the same from all angles
+
+			// var spriteMaterial = new THREE.SpriteMaterial( 
+			// { 
+			// 	map: new THREE.ImageUtils.loadTexture( 'images/glow.png' ), 
+			// 	useScreenCoordinates: false, alignment: THREE.SpriteAlignment.center,
+			// 	color: 0x0000ff, transparent: false, blending: THREE.AdditiveBlending
+			// });
+			// var sprite = new THREE.Sprite( spriteMaterial );
+			// sprite.scale.set(200, 200, 1.0);
+			// mesh.add(sprite); // this centers the glow at the mesh
 		}
 	}
 }();
