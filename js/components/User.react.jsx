79d4bfcd82080fcda0ce53edc4f16b68d717dfaf
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2015-09-20 18:21:49 +0200

    added the game and spirits and game now works fine with a set time for movement. Maybe add a millisecond on the api so that you can choose how much you want to move

--- js/components/User.react.jsx
+++ js/components/User.react.jsx
@@ -1,27 +1,27 @@
 var React = require('react');
+var PointUpdater = require('../mixins/PointUpdater.jsx')
 
 var User = React.createClass({
+    mixins: [PointUpdater],
     componentDidMount: function() {
         console.log("component did mount")
+        this.trackPoints(this.props.user);
     },
-
     getInitialState: function() {
+        return {}
+    },
+    getDefaultProps: function() {
         return {
-            user: {}
+            user: 'No users'
         }
     },
 
     render: function() {
         return (
-            this.props.user ?
-                <div className='user'>
-                    <span>{this.props.user}</span>
-                    <span> - 0 points</span>
-                </div>
-                :
-                <div class='user'>
-                   No users
-               </div>
+            <div className='user'>
+                <span>{this.props.user}</span>
+                <span>{this.state.points ? ' '+ this.state.points +' points' : ''}</span>
+            </div>
         );
     }
 
